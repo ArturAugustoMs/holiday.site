6fcf60dfa61247e93aa4999ed942f935c7b12cf3
ArturAugustoMs <artur.augusto.ms@gmail.com>
2026-02-18 16:15:57 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-import HolidayCampSection from "@/components/HolidayCampSection";
 import AboutSection from "@/components/AboutSection";
+import HolidayCampSection from "@/components/HolidayCampSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 
